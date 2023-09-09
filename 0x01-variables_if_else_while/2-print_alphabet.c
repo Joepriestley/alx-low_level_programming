28d3616771a9e2a0a6ecca997f6b5fe74284de3b
Joepriestley <joeamonoo1114@gmail.com>
2023-09-09 03:27:21 -0700

    printing the alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main is the maind block of code
+ *
+ * Return: returns 0 when code  runs
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
