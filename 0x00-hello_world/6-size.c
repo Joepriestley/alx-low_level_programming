c228dffd7c4788dc43471d8e44cadce190e01b83
Joepriestley <joeamonoo1114@gmail.com>
2023-09-08 17:14:28 -0700

    The sizeof function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - it is the main block of code 
+ * main - this is the main function
  *
- * Return: returns 0 with success 
+ * Return: 0 when successful
  */
 int main(void)
 {
-	printf("Size of a char:%lu byte(s)\n",sizeof(char));
-	printf("Size of an int:%lu byte(s)\n",sizeof(int));
-	printf("Size of a long int:%lu byte(s)\n",sizeof(long int));
-	printf("Size of a long long int:%lu byte(s)\n",sizeof(long long int));
-	printf("Size of a float:%lu byte(s)\n",sizeof(float));
-	return(0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
