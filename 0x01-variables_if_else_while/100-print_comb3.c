3f64012b985d09b2a1fff3e706fe51eba66ac66c
Joepriestley <joeamonoo1114@gmail.com>
2023-09-10 21:35:48 -0700

    two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,24 @@
  */
 int main(void)
 {
-	int a;
-	int i;
+	int a, i;
 
 	for (a = 48 ; a <= 56 ; a++)
 	{
 		for (i = 49 ; i <= 57 ; i++)
 		{
-			putchar(a);
-			putchar(i);
-			if (a != 56 || i != 57)
+			if (i > a)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(a);
+				putchar(i);
 
+				if (a != 56 || i != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-
 	}
 	putchar('\n');
 	return (0);
