e2fc3271104aeded279874b9f29e4ccf398ae463
Joepriestley <joeamonoo1114@gmail.com>
2023-09-10 17:56:57 -0700

    base 10 single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block of code
+ *
+ * Return: returns (0) for success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+		printf("%d \n", i);
+
+	return (0);
+}
