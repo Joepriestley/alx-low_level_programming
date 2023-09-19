5e3dea8d1767bb58282159eba5d49b0f8fcd47f0
Joepriestley <joeamonoo1114@gmail.com>
2023-09-18 20:25:12 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: the string to be printed
+ * Return: returns void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; str[i
+}
