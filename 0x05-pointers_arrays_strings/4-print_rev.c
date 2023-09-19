9f9ec0062558cfa4ec5e05ce5fa99d04a53b1bdb
Joepriestley <joeamonoo1114@gmail.com>
2023-09-18 21:33:15 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,5 +11,9 @@ void print_rev(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0 ; str[i
+	for (i = 0 ; str[i] != '\0' ; i++)
+		count++;
+	for (i = count - 1 ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
